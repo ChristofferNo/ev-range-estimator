469747b5fc5b81d73ecb08a29c439c18ca5f394e
viktorsnyman <viktornyman@yahoo.com>
2026-02-11 14:01:14 +0100

    hej

--- script_part2c.py
+++ script_part2c.py
@@ -4,19 +4,20 @@ import roadster
 import matplotlib.pyplot as plt
 
 def konvergensstudie(route, antal_n, x=65):
-    värden = []
-    n=50000
+    felvärden = []
+    n=1
     list_n = []
     förväntat = []
-    for i in range(antal_n):
-        b = 2**i
-        list_n.append(b)
-        värden.append(abs((roadster.time_to_destination(x, route, b*n))- 0.9609402397659929))
-        förväntat.append(1/(n))
-    plt.loglog(list_n, värden)
+    while n < 500000000:
+        
+        list_n.append(n)
+        felvärden.append(abs((roadster.time_to_destination(x, route, n))- 0.9609402397659929))
+        förväntat.append(1/((n)**2))
+        n=2*n
+    plt.loglog(list_n, felvärden)
     plt.plot(list_n, förväntat)
     plt.show()
-    return värden[-1]
+    return felvärden[-1]
 print(konvergensstudie('speed_elsa.npz', 8))
 
 
